61707ee277ccca4a4ada5e23e2dfb65efeac2f41
Lauren <lemherdman@hotmail.com>
2015-10-26 16:50:29 -0400

    exercise2

---
+++ exercise2.rb
@@ -0,0 +1,19 @@
+##Lauren Herdman
+#Ruby Fundamentals Part 1
+#Exercise 2
+
+puts "1."
+puts "A good tip for a 55 dollar meal would be "
+puts 55*0.15
+
+puts "2."
+puts "This is a string " + 12345.to_s
+
+puts "3."
+a = 45626*7839
+puts "45626*7839 = #{a}" 
+
+puts "4."
+puts "What I think the value is: true"
+puts "What the value actually is: "
+puts (true && false) || (false && true) || !(false&&false)
